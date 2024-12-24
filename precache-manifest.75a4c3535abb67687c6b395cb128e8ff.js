f7eed729eeb55c5043888171f52c1a9afead581a
ZUBAIR <zubairtahir591@gmail.com>
2024-12-24 20:12:24 +0500

    Updates

---
+++ precache-manifest.75a4c3535abb67687c6b395cb128e8ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad61af2f6ec07cde93a9cef78ec94198",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "83ede620ec57933614fa",
+    "url": "/portfolio/static/css/main.846d34c8.chunk.css"
+  },
+  {
+    "revision": "fc3dbba0514b0284f44e",
+    "url": "/portfolio/static/js/2.54d83443.chunk.js"
+  },
+  {
+    "revision": "9836b0e3e9deaac4bd51ae5d8d877206",
+    "url": "/portfolio/static/js/2.54d83443.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83ede620ec57933614fa",
+    "url": "/portfolio/static/js/main.0c96b1f7.chunk.js"
+  },
+  {
+    "revision": "11df3bdc8ce778e78bb2",
+    "url": "/portfolio/static/js/runtime-main.e316a265.js"
+  }
+]);
