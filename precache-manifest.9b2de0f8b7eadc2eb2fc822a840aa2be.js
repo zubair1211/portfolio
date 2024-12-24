ab8a2a5efe3da6704c3871a6e1f4f3abd34ff663
ZUBAIR <zubairtahir591@gmail.com>
2024-12-24 20:43:20 +0500

    Updates

--- precache-manifest.9b2de0f8b7eadc2eb2fc822a840aa2be.js
+++ precache-manifest.9b2de0f8b7eadc2eb2fc822a840aa2be.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ad61af2f6ec07cde93a9cef78ec94198",
+    "revision": "93e26aca80ff7c67a9d94599919072f1",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "83ede620ec57933614fa",
+    "revision": "72b1b96fa9f24aa1a591",
     "url": "/portfolio/static/css/main.846d34c8.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.54d83443.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "83ede620ec57933614fa",
-    "url": "/portfolio/static/js/main.0c96b1f7.chunk.js"
+    "revision": "72b1b96fa9f24aa1a591",
+    "url": "/portfolio/static/js/main.7474efc5.chunk.js"
   },
   {
     "revision": "11df3bdc8ce778e78bb2",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.75a4c3535abb67687c6b395cb128e8ff.js"
+  "/portfolio/precache-manifest.9b2de0f8b7eadc2eb2fc822a840aa2be.js"
 );
 
 self.addEventListener('message', (event) => {
